acc969085e7b981888f28ed034aa6452d7c815b6
mvcobos <vcobos178@gmail.com>
2025-01-24 12:25:45 -0500

    ADD: Sets

--- src/App.java
+++ src/App.java
@@ -1,5 +1,50 @@
+import java.util.Set;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        runHashSet();     
+        runLinkedHashSet();      
+        runTreeSet();      
+        runTreeSetComparado();                                           
+    }
+
+    public static void runHashSet() {
+        Sets sets = new Sets();
+        Set<String> ejemploHashSet = sets.construirHashSet();
+        System.out.println("-------- HashSet --------");
+        System.out.println("Elementos del HashSet (no se garantiza orden):");
+        for( String elemento : ejemploHashSet){
+            System.out.println(elemento);
+        }
+    }
+
+    public static void runLinkedHashSet(){
+        Sets sets = new Sets();
+        Set<String> ejemploHashSet = sets.construirLinkedHashSet();
+        System.out.println("-------- LinkedHashSet --------");
+        System.out.println("Elementos del LinkedHashSet (Respeta orden de insercion):");
+        for( String elemento : ejemploHashSet){
+            System.out.println(elemento);
+        }
+    }
+
+    public static void runTreeSet() {
+        Sets sets = new Sets();
+        Set<String> ejemploTreeSet = sets.construirTreeSet();
+        System.out.println("-------- TreeSet --------");
+        System.out.println("Elementos del TreeSet (Orden alfabetico):");
+        for( String elemento : ejemploTreeSet){
+            System.out.println(elemento);
+        }
+    }
+
+    public static void runTreeSetComparado() {
+        Sets sets = new Sets();
+        Set<String> ejemploTreeSetComparado = sets.construirTreeSetConComparador();
+        System.out.println("-------- TreeSet con Comparador --------");
+        System.out.println("Elementos del TreeSet (Orden por longitud y luego alfabeticamente (Uso de metodo sobreEscrito)):");
+        for( String elemento : ejemploTreeSetComparado){
+            System.out.println(elemento);
+        }
     }
 }
---
+++ src/Sets.java
@@ -0,0 +1,87 @@
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sets {
+    //HashSet es un conjuto de elementos que NO mantiene orden
+    public Sets(){
+
+    }
+
+    //LinkedHashet mantiene el orden
+    //Clases abstractas: Clases que no pueden ser instanciadas
+
+    public Set<String> construirHashSet(){
+        Set<String> miHashSet = new HashSet<>();
+
+        miHashSet.add("Manzana");
+        miHashSet.add("Pera");
+        miHashSet.add("Pera");
+        miHashSet.add("Melocoton");
+        miHashSet.add("Melon");
+        miHashSet.add("Kiwi");
+        miHashSet.add("Fresa");
+
+        return miHashSet;
+    }
+
+    public Set<String> construirLinkedHashSet(){
+        Set<String> miLinkedHashSet = new LinkedHashSet<>();
+
+        miLinkedHashSet.add("Manzana");
+        miLinkedHashSet.add("Pera");
+        miLinkedHashSet.add("Pera");
+        miLinkedHashSet.add("Melocoton");
+        miLinkedHashSet.add("Melon");
+        miLinkedHashSet.add("Kiwi");
+        miLinkedHashSet.add("Fresa");
+
+        return miLinkedHashSet;
+    }
+
+    public Set<String> construirTreeSet(){
+        Set<String> miTreeSet = new TreeSet<>();
+
+        miTreeSet.add("Manzana");
+        miTreeSet.add("Pera");
+        miTreeSet.add("Pera");
+        miTreeSet.add("Melocoton");
+        miTreeSet.add("Melon");
+        miTreeSet.add("Kiwi");
+        miTreeSet.add("Fresa");
+
+        return miTreeSet;
+    }
+
+    public Set<String> construirTreeSetConComparador(){
+
+        Comparator<String> comparatorPorLongitud = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2){
+            //Primero comparamos la longitud
+            int resultado = Integer.compare(s1.length(), s2.length());
+            //Si tienen la misma longitud, comparamos alfabeticamente
+            if(resultado == 0){
+                resultado = s1.compareTo(s2);
+            }
+            return resultado;
+            }
+        };
+
+        Set<String> miTreeSetComparador = new TreeSet<>(comparatorPorLongitud);
+
+        miTreeSetComparador.add("Manzana");
+        miTreeSetComparador.add("Pera");
+        miTreeSetComparador.add("Pera");
+        miTreeSetComparador.add("Melocoton");
+        miTreeSetComparador.add("Melon");
+        miTreeSetComparador.add("Kiwi");
+        miTreeSetComparador.add("Fresa");
+
+        return miTreeSetComparador;
+
+
+    }
+}
